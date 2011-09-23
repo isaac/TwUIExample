be944a19420ea43ca5e4d965cacd148d92373609
Isaac Kearse <isaac@kearse.co.nz>
2011-09-23 12:46:35 +1200

    Add ExampleTabBar view

---
+++ lib/views/ExampleTabBar.rb
@@ -0,0 +1,15 @@
+class ExampleTabBar < TUIView
+  attr_accessor :delegate, :tabViews
+
+  def initWithNumberOfTabs tabs
+    initWithFrame CGRectZero
+    self.tabViews = []
+    (0..tabs - 1).to_a.each do |index|
+      t = ExampleTab.alloc.initWithFrame CGRectZero
+      t.setLayout lambda { |v| CGRectZero }
+      addSubview t
+      tabViews << t
+    end
+    self
+  end
+end
