c9ff50a94f9840db5dd672ec4c34842fdd5beba6
Isaac Kearse <isaac@kearse.co.nz>
2011-10-30 11:36:48 +1300

    Remove menu noise

--- lib/application.rb
+++ lib/application.rb
@@ -20,30 +20,6 @@ class TwUIExample
       end
     end
   end
-
-  # file/open
-  def on_open(menu)
-  end
-
-  # file/new
-  def on_new(menu)
-  end
-
-  # help menu item
-  def on_help(menu)
-  end
-
-  # This is commented out, so the minimize menu item is disabled
-  #def on_minimize(menu)
-  #end
-
-  # window/zoom
-  def on_zoom(menu)
-  end
-
-  # window/bring_all_to_front
-  def on_bring_all_to_front(menu)
-  end
 end
 
 TwUIExample.new.start
