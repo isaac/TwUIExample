a7fc0c54fb6f1e9f0a0a4ef4c919aec9cba2aa8c
Isaac Kearse <isaac@kearse.co.nz>
2011-10-30 11:36:07 +1300

    Remove layout block workaround

--- lib/views/ExampleTab.rb
+++ lib/views/ExampleTab.rb
@@ -14,16 +14,4 @@ class ExampleTab < TUIView
     TUIView.animateWithDuration 0.5, animations:animations
     tabBar.delegate.tabBar(tabBar, didSelectTab:self.tag) if eventInside(event)
   end
-
-  def layoutSubviews
-    subviews.each do |v|
-      b = v.superview.bounds
-      tabs = tabBar.tabs
-      puts tabs
-      puts tabs.class
-      width = b.size.width / tabs
-      x = index * width
-      v.frame = [ x.round, 0, width.round, b.size.height ]
-    end
-  end
 end
